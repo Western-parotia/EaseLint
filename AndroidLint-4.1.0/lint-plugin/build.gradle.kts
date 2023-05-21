31eaf915caa78c0008f31499bea1cb58e14d5931
zhushiwen <sink50@foxmail.com>
2023-05-21 21:37:54 +0800

    移除7.0+的配置

--- AndroidLint-4.1.0/lint-plugin/build.gradle.kts
+++ AndroidLint-4.1.0/lint-plugin/build.gradle.kts
@@ -1,8 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 com.buildsrc.easelint.GlobalConfig.init(project)
 plugins {
-    id("com.android.application") version "7.1.3" apply false
-    id("org.jetbrains.kotlin.android") version "1.6.20" apply false
     `maven-publish`
 }
 buildscript {
