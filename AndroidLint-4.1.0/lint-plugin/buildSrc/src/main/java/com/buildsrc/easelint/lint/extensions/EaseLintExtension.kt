68d8b5e58bf88a32df84a42d71809aec632f5d41
lijingjiu <lijingjiu123@163.com>
2023-05-31 16:27:19 +0800

    目标文件增加后缀判断

--- AndroidLint-4.1.0/lint-plugin/buildSrc/src/main/java/com/buildsrc/easelint/lint/extensions/EaseLintExtension.kt
+++ AndroidLint-4.1.0/lint-plugin/buildSrc/src/main/java/com/buildsrc/easelint/lint/extensions/EaseLintExtension.kt
@@ -1,6 +1,7 @@
 package com.buildsrc.easelint.lint.extensions
 
 import java.util.*
+
 /**
  * 在module的build.gradle中配置 easeLint 参数
  */
@@ -18,11 +19,36 @@ open class LintConfigExtension {
     //扫描文件白名单,有些文件始终都不需要被扫描
     var fileWhiteList: LinkedList<String> = LinkedList()
 
+    internal val gitDiffConfig = GitDiffConfig()
+
+    /**
+     * 设置查找目标文件（git diff）的参数
+     *
+     * @param compareBranch 对比指定分支，默认对比远程master分支
+     * @param compareCommitId 对比指定的提交记录
+     * @param targetFileSuffix 目标文件后缀
+     */
+    fun setGitDiffConfig(
+        compareBranch: String,
+        compareCommitId: String,
+        targetFileSuffix: List<String> = emptyList()
+    ) {
+        gitDiffConfig.compareBranch = compareBranch
+        gitDiffConfig.compareCommitId = compareCommitId
+        if (targetFileSuffix.isNotEmpty())
+            gitDiffConfig.targetFileSuffix = targetFileSuffix
+    }
+
+}
+
+class GitDiffConfig {
     //通过git获取目标文件，对比指定分支，默认对比远程master分支
     var compareBranch: String = ""
 
     //通过git获取目标文件，对比指定的提交记录
     var compareCommitId: String = ""
 
+    //目标文件的后缀
+    var targetFileSuffix: List<String> = listOf("java", "kt", "xml")
 }
 
