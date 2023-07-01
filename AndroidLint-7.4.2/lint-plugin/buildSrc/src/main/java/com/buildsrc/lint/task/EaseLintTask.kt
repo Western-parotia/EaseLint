3ea03f8cede610b76e8514c43e9f29a2ac38f6f4
juziml <harkben@gmail.com>
2023-07-01 21:45:37 +0800

    测试 僵尸文件 通过

--- AndroidLint-7.4.2/lint-plugin/buildSrc/src/main/java/com/buildsrc/lint/task/EaseLintTask.kt
+++ AndroidLint-7.4.2/lint-plugin/buildSrc/src/main/java/com/buildsrc/lint/task/EaseLintTask.kt
@@ -12,19 +12,19 @@ abstract class EaseLintTask : DefaultTask() {
     companion object {
         const val TASK_NAME = "a1EaseLint"
         private val targetFiles: JvmWideVariable<ArrayList<String>> =
-            JvmWideVariable(
-                LintHookHelper.lintRequestClass,
+            JvmWideVariable(LintHookHelper.lintRequestClass,
                 "targetFiles",
-                object : TypeToken<ArrayList<String>>() {}
-            ) { ArrayList() }
+                object : TypeToken<ArrayList<String>>() {}) { ArrayList() }
     }
 
     /**
      * 如果没有获取到文件，那么使用一个僵尸文件进行替代，避免project.subset 没有文件而进行全量扫描
      */
     private val zombieFile by lazy {
-        val file = File(project.buildDir, "easeLint.zombie")
+        var file = File(project.buildDir, "easeLint.zombie")
         if (!file.exists()) {
+            val parent = File(project.buildDir.absolutePath)
+            parent.mkdir()
             file.createNewFile()
         }
         file.absolutePath
